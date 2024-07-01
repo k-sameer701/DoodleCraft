e817926c35fd3189d281a1a88ce8998a205e3f46
k-sameer701 <sameer2001kr@gmail.com>
2024-07-01 19:20:29 +0530

    Release Fixed

--- app/src/main/java/com/example/doodlecraft/MainActivity.kt
+++ app/src/main/java/com/example/doodlecraft/MainActivity.kt
@@ -3,15 +3,7 @@ package com.example.doodlecraft
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.doodlecraft.screens.DrawingScreen
-import com.example.doodlecraft.ui.theme.DoodleCraftTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
